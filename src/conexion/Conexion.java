a8b487831cc245719bffbf186eab94b3d703493c
joseph mora <david.mora65@uabc.edu.mx>
2019-10-09 11:20:10 -0700

    Primera version login usuario, creacion y eliminacion de productos

---
+++ src/conexion/Conexion.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author josep
+ */
+public class Conexion {
+    
+    connection conectar=null;
+    
+    public Connection conexion(){
+        return null;
+        
+    }
+    
+}
